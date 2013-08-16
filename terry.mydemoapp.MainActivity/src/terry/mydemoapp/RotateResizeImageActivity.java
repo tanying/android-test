84252c8a4e211d1235991c89f5aba55d31e20def
ying.tan <ying.tan@tcl.com>
2013-08-16 14:48:11 +0800

    try again

--- terry.mydemoapp.MainActivity/src/terry/mydemoapp/RotateResizeImageActivity.java
+++ terry.mydemoapp.MainActivity/src/terry/mydemoapp/RotateResizeImageActivity.java
@@ -16,7 +16,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-
+// try again
 public class RotateResizeImageActivity extends Activity{
     
     Context mContext = null;
